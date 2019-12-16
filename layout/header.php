3dec84c1c88dc5299c16ddb517693d6171117800
Dima <15dimon03@gmail.com>
2019-12-16 23:17:56 +0200

    перевірка загрузки фото рецепту

--- layout/header.php
+++ layout/header.php
@@ -16,6 +16,8 @@
     <!-- Site Title -->
     <title>Culinary blog</title>
 
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
     <!--
     CSS
@@ -104,7 +106,7 @@
                                                     </li>
                                                     <li>
                                                         <form method = "get">
-                                                                <a href="?dishtype=Другі Страви&action=bydishtype" name="action" value="Другі Страви">Другі Страви</a>   
+                                                                <a href="?dishtype=Другі Страви&action=bydishtype" name="action" value="Другі Страви">Другі Страви</a>
                                                             </form>
                                                         </li>
                                                         <li>
